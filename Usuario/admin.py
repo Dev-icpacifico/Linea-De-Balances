48b798f31527f886954988c027e9562e2831ae46
Dev-icpacifico <desarrolloeinnovacion@icpacifico.cl>
2024-11-19 16:55:22 -0300

    Feat: Se implementó la libreria Jazzmin para customizar el panel admin

--- Usuario/admin.py
+++ Usuario/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from Usuario.models import Usuario
+from django.contrib.auth.models import Permission
 
-# Register your models here.
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'nombres',
+        'apellidos',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+
+    )
+    search_fields=['id_usuario']
+
+
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Permission)
